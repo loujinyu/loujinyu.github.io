f8b61bab731d5b08b08660c656b775fd6b2c7cbb
loujinyu <17839831622@163.com>
2026-01-08 15:22:27 +0800

    Site updated: 2026-01-08 15:22:26

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["posts/970f.html","posts/1eaa.html","posts/123c.html","posts/337a.html","posts/536e.html","posts/2cd7.html"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
