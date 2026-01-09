cb2405117f1a79a22b93ae8dcae8d16812672d17
loujinyu <17839831622@163.com>
2026-01-09 09:36:01 +0800

    Site updated: 2026-01-09 09:36:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/970f.html","posts/1eaa.html","posts/123c.html","posts/337a.html","posts/536e.html","posts/2cd7.html"];function toRandomPost(){
+var posts=["posts/970f.html","posts/123c.html","posts/1eaa.html","posts/337a.html","posts/536e.html","posts/2cd7.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
