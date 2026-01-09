df3ce688c8e58e97d383b0416840de72c957d4f5
loujinyu <17839831622@163.com>
2026-01-09 15:34:27 +0800

    Site updated: 2026-01-09 15:34:26

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/970f.html","posts/629f.html","posts/f723.html","posts/1eaa.html","posts/337a.html","posts/123c.html","posts/536e.html","posts/2cd7.html","posts/cf95.html"];function toRandomPost(){
+var posts=["posts/970f.html","posts/629f.html","posts/f723.html","posts/1eaa.html","posts/337a.html","posts/123c.html","posts/536e.html","posts/2cd7.html","posts/cf95.html","posts/8b7.html","posts/d1d.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
